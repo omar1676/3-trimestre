98961f0d9bda1a9728ba956c3ac702c623358eaf
Alvaro Brasero <alvarobrasero8@gmail.com>
2026-05-23 18:10:00 +0200

    feat: add application entry point

---
+++ java/src/com/omar/Main.java
@@ -0,0 +1,22 @@
+package com.omar;
+
+import com.omar.interfaz.Menu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        mostrarBienvenida();
+        Menu menu = new Menu();
+        menu.start();
+        System.out.println("Gracias por usar el CRM. Hasta pronto!");
+    }
+
+    private static void mostrarBienvenida() {
+        System.out.println("+------------------------------------------+");
+        System.out.println("|                                          |");
+        System.out.println("|          CRM - INSTITUTO XTART           |");
+        System.out.println("|       Gestion de alumnos y cursos        |");
+        System.out.println("|                                          |");
+        System.out.println("+------------------------------------------+");
+    }
+}
